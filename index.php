5bf36c8530a527fc0d081bc313ba31d570f7357a
4ks <107892265+4ks-devops@users.noreply.github.com>
2023-02-27 14:45:32 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<?php require_once "vistas/parte_superior.php"?>
-
 <!--INICIO del cont principal-->
 <div class="container">
     <h1>Página de buttons</h1>
@@ -29,4 +27,4 @@
 </div>
 <!--FIN del cont principal-->
 
-<?php require_once "vistas/parte_inferior.php"?>
+
